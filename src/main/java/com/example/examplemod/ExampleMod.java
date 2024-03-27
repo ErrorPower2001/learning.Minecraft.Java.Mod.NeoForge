6fc9dce909b510cc3bff586a3fef45d3b326a87a
ErrorPower2001 <ErrorPower2001@outlook.com>
2024-03-27 15:40:53 +0800

    fix: 更改 Example Mod 中的 modid 检测匹配

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -36,7 +36,8 @@ import org.slf4j.Logger;
 public class ExampleMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    //..public static final String MODID = "examplemod";
+    public static final String MODID = "ep_learning_neoforge_mod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
